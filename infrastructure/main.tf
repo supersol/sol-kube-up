c964c4d312a920e28eb991d1d8f7b8a8cf7a0de9
Rustam Khutov <rustam_khutov@epam.com>
2019-01-05 14:14:20 +0300

    add intances

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,16 @@
 # export GOOGLE_APPLICATION_CREDENTIALS="[PATH_TO_CREDS/SOME_CREDS.json]"
 # export GOOGLE_PROJECT="YOUR_PROJECT_NAME"
-# b,c,d
 provider "google" {
-  region      = "europe-west1"
+  region = "europe-west1"
+}
+
+variable "node_count" {
+  default = "3"
+}
+
+variable "zones" {
+  type    = "list"
+  default = ["europe-west1-b", "europe-west1-c", "europe-west1-d"]
 }
 
 resource "google_compute_network" "kube_network" {
@@ -12,14 +20,15 @@ resource "google_compute_network" "kube_network" {
 
 resource "google_compute_subnetwork" "kube_subnetwork" {
   ip_cidr_range = "10.240.0.0/24"
-  name          = "${terraform.workspace}-kube-network-range"
+  name          = "${terraform.workspace}-kube-network-subnet"
   network       = "${google_compute_network.kube_network.self_link}"
 }
 
 
 resource "google_compute_firewall" "kube-internal" {
-  name = "kube-firewall-internal"
-  network = "${google_compute_network.kube_network.self_link}"
+  name          = "kube-firewall-internal"
+  network       = "${google_compute_network.kube_network.self_link}"
+  source_ranges = ["10.240.0.0/24", "10.200.0.0/16"]
 
   allow {
     protocol = "icmp"
@@ -33,12 +42,12 @@ resource "google_compute_firewall" "kube-internal" {
     protocol = "tcp"
   }
 
-  source_ranges = ["10.240.0.0/24", "10.200.0.0/16"]
 }
 
 resource "google_compute_firewall" "kube-external" {
-  name = "kube-firewall-external"
-  network = "${google_compute_network.kube_network.self_link}"
+  name          = "kube-firewall-external"
+  network       = "${google_compute_network.kube_network.self_link}"
+  source_ranges = ["0.0.0.0/0"]
 
   allow {
     protocol = "tcp"
@@ -49,5 +58,63 @@ resource "google_compute_firewall" "kube-external" {
     protocol = "icmp"
   }
 
-  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "ip_address" {
+  name = "kube-ip-address"
+}
+
+resource "google_compute_instance" "kube-controller-" {
+  count          = "${var.node_count}"
+  name           = "${terraform.workspace}-kube-controller-${count.index}"
+  machine_type   = "n1-standard-1"
+  zone           = "${var.zones["${count.index}"]}"
+  tags           = ["${terraform.workspace}-kubernetes", "controller"]
+
+  can_ip_forward = "true"
+
+  boot_disk {
+    initialize_params {
+      size = 200
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.kube_subnetwork.self_link}"
+    address = "10.240.0.1${count.index}"
+  }
+
+  service_account {
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
+  }
+}
+
+resource "google_compute_instance" "kube-worker-" {
+  count          = "${var.node_count}"
+  name           = "${terraform.workspace}-kube-worker-${count.index}"
+  machine_type   = "n1-standard-1"
+  zone           = "${var.zones["${count.index}"]}"
+  tags           = ["${terraform.workspace}-kubernetes", "worker"]
+  can_ip_forward = "true"
+
+  boot_disk {
+    initialize_params {
+      size  = 200
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  metadata {
+    pod-cidr = "10.200.${count.index}.0/24"
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.kube_subnetwork.self_link}"
+    address    = "10.240.0.2${count.index}"
+  }
+
+  service_account {
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
+  }
 }
